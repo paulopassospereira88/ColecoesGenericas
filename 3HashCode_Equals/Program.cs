52b8cc2d1253a45fe24c64ad3d48b4e4369b7f63
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-12-13 15:34:53 -0300

    hashCode e equals

---
+++ 3HashCode_Equals/Program.cs
@@ -0,0 +1,45 @@
+﻿string a = "teste";
+string b = "teste";
+
+int x = 100;
+int y = 100;
+
+Console.WriteLine(a.Equals(b));
+Console.WriteLine(x.Equals(y));
+Console.WriteLine();
+Console.WriteLine(x.GetHashCode());
+Console.WriteLine(y.GetHashCode());
+
+var p1 = new Pessoa(1, "teste");
+var p2 = new Pessoa(1, "teste");
+
+Console.WriteLine(p1.Equals(p2));
+
+Console.WriteLine(p1.GetHashCode());
+Console.WriteLine(p2.GetHashCode());
+
+Console.ReadKey();
+
+public class Pessoa
+{
+    private int id;
+    private string? nome;
+
+    public Pessoa(int id, string? nome)
+    {
+        this.Id = id;
+        this.Nome = nome;
+    }
+
+    public int Id
+    {
+        get { return id; }
+        set { id = value; }
+    }
+
+    public string Nome
+    {
+        get { return nome; }
+        set { nome = value; }
+    }
+}

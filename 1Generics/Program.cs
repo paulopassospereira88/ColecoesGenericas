fc0a2d2f25dcf504caa3f1a89195f2be0179aa2c
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-12-13 14:58:45 -0300

    Add project files.

---
+++ 1Generics/Program.cs
@@ -0,0 +1,32 @@
+﻿Teste<int, int> teste = new Teste<int, int>();
+//Teste<string, string> teste1 = new Teste<string, string>();
+
+int p1 = 10;
+int p2 = 10;
+
+float f1 = 4.5f;
+
+string s1 = "teste";
+string s2 = "teste";
+
+teste.Comparar(p1, p2);
+teste.Comparar(s1, s2);
+teste.Comparar(p1, f1);
+
+public class Teste<T1, T2> where T1: struct where T2 : struct
+{
+
+    public void Comparar<T1, T2>(T1 p1, T2 p2)
+    {
+        Console.WriteLine($"Os tipos : {p1.GetType()} e {p2.GetType()}\n");
+        var result = p1.Equals(p2);
+        Console.WriteLine($"{p1} e {p2} sao iguais ? {result}");
+    }
+
+    //public void Comparar<T>(T p1, T p2)
+    //{
+    //    Console.WriteLine($"Os tipos : {p1.GetType()} e {p2.GetType()}\n");
+    //    var result = p1.Equals(p2);
+    //    Console.WriteLine($"{p1} e {p2} sao iguais ? {result}");
+    //}
+}
---
+++ 2Generics/Program.cs
@@ -0,0 +1,47 @@
+﻿ClasseGenerica<int> intObj = new ClasseGenerica<int>();
+ClasseGenerica<string> stringObj = new ClasseGenerica<string>();
+
+intObj.Adicionar(10);
+intObj.Adicionar(20);
+intObj.Adicionar(30);
+intObj.Adicionar(40);
+intObj.Adicionar(50);
+intObj.Adicionar(60);
+
+stringObj.Adicionar("teste 1");
+stringObj.Adicionar("teste 2");
+stringObj.Adicionar("teste 3");
+
+Console.WriteLine("Classe ClasseGenerica<M> de objetos int\n");
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine(intObj[i]);
+}
+
+Console.WriteLine("Classe ClasseGenerica<M> de objetos string");
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(stringObj[i]);
+}
+
+
+public class ClasseGenerica<T>
+{
+    T[] obj = new T[5];
+    int contador = 0;
+
+    public void Adicionar(T item)
+    {
+        if (contador < 5)
+        {
+            obj[contador] = item;
+        }
+        contador++;
+    }
+
+    public T this[int index]
+    {
+        get { return obj[index]; }
+        set { obj[index] = value; }
+    }
+}

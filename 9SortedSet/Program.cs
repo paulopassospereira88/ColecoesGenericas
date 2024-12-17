a42d3db7e3afad04ed23dc2db63b4e6ae08915fb
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-12-17 09:45:57 -0300

    conjunto SortedSet

---
+++ 9SortedSet/Program.cs
@@ -0,0 +1,7 @@
+﻿var numeros = new List<int>() {50, 20, 10, 80, 70, 60};
+SortedSet<int> conjunto = new SortedSet<int>(numeros);
+
+foreach (var VARIABLE in conjunto)
+{
+    Console.WriteLine(VARIABLE);
+}
